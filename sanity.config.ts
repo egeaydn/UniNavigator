abb021b836a7ba88fb972d6723547490866c5014
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2025-09-02 18:33:31 +0300

    feat: remove Sanity Studio integration and related configuration files

---
+++ sanity.config.ts
@@ -0,0 +1,8 @@
+'use client'
+
+/**
+ * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
+ */
+
+// Sanity entegrasyonu kaldırıldı. Bu dosya artık kullanılmıyor.
+export default {};
